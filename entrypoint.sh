753ea942f768492173ea43ff1994b61be4b89bc6
Mars Lan <mars.th.lan@gmail.com>
2021-08-24 12:48:21 -0700

    Generate YAML-based config file and update README (#3)

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,12 @@
 
 set -e -x
 
-cat << EOF > dbt.json
-{
-  "manifest": "/github/workspace/$1",
-  "catalog": "/github/workspace/$2",
-  "output": {
-    "file": {
-      "path": "$3"
-    }
-  }
-}
+cat << EOF > dbt.yaml
+manifest: "/github/workspace/$1",
+catalog: "/github/workspace/$2",
+output:
+  file:
+    path: "$3"
 EOF
 
-python -m metaphor.dbt dbt.json
+python -m metaphor.dbt dbt.yaml
