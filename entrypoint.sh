8bc85e3432a4d44d3acaae85ec1573e02171e971
Mars Lan <mars.th.lan@gmail.com>
2021-08-24 13:18:33 -0700

    Drop the trailing comma (#4)

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 set -e -x
 
 cat << EOF > dbt.yaml
-manifest: "/github/workspace/$1",
-catalog: "/github/workspace/$2",
+manifest: "/github/workspace/$1"
+catalog: "/github/workspace/$2"
 output:
   file:
     path: "$3"
