1f8227e67008ef07b452ab36d4cc81ca1532d88e
Yi (Alan) Wang <yi@metaphor.io>
2021-09-07 13:56:44 -0700

    Update DBT action and DBT github workflow [ch3433] (#5)
    * Update DBT action and DBT github workflow [ch3433]
    
    * address comment

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,10 @@ set -e -x
 cat << EOF > dbt.yaml
 manifest: "/github/workspace/$1"
 catalog: "/github/workspace/$2"
+account: "$3"
 output:
   file:
-    path: "$3"
+    path: "$4"
 EOF
 
 python -m metaphor.dbt dbt.yaml
